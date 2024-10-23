e4e463d4559998e17a4844307428810957d1a82b
Akilesh Sethu <asethu19@gmail.com>
2024-10-23 09:33:00 +0800

    go func and select - fun and no profit

--- internal/domain/recipe/memory.go
+++ internal/domain/recipe/memory.go
@@ -3,6 +3,7 @@ package recipe
 import (
 	"context"
 	"sync"
+	"time"
 
 	"github.com/google/uuid"
 )
@@ -17,11 +18,35 @@ func NewMemoryRepository() *MemoryRepository {
 		recipes: make(map[uuid.UUID]Recipe),
 	}
 }
+
+type wrapper struct {
+	recipe Recipe
+	err    error
+}
+
 func (mr *MemoryRepository) Get(ctx context.Context, id uuid.UUID) (Recipe, error) {
-	if recipe, ok := mr.recipes[id]; ok {
-		return recipe, nil
+	select {
+	case <-ctx.Done():
+		return Recipe{}, ctx.Err()
+	case res := <-mr.get(id):
+		return res.recipe, res.err
 	}
-	return Recipe{}, ErrRecipeNotFound
+}
+
+// this really isnt needed. just for fun
+func (mr *MemoryRepository) get(id uuid.UUID) <-chan wrapper {
+	ch := make(chan wrapper)
+	go func() {
+		time.Sleep(200 * time.Millisecond)
+		mr.mu.Lock()
+		defer mr.mu.Unlock()
+		if recipe, ok := mr.recipes[id]; ok {
+			ch <- wrapper{recipe: recipe}
+			return
+		}
+		ch <- wrapper{err: ErrRecipeNotFound}
+	}()
+	return ch
 }
 
 func (mr *MemoryRepository) Add(recipe Recipe) error {
